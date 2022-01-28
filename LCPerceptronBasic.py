c6e608c07a8e31e0bb8a99381c5a414c58ba2c83
TYongK <93954052+TYongK@users.noreply.github.com>
2022-01-27 22:33:55 -0500

    Update LCPerceptronBasic.py

--- LCPerceptronBasic.py
+++ LCPerceptronBasic.py
@@ -30,27 +30,40 @@ def DataOrganize(Dataset):
     dataset_labels = Dataset[:,-1][np.newaxis].T
     return dataset_features, dataset_labels
 
-def ShowGraphAnimation (Result_th, Result_thZero, Dataset, iteration):
+def SaveGraphAnimationImages (Result_th, Result_thZero, Dataset, iteration):
     dataset_features, dataset_labels = DataOrganize(Dataset)
-    
-    print(np.where(dataset_labels[:,0] == 1))
     
     positive_features = dataset_features[np.where(dataset_labels[:,0] == 1)]
     negative_features = dataset_features[np.where(dataset_labels[:,0] == -1)]
     
     xOne = np.linspace(-1000,3500)
-    xTwo = -1*(xOne*Result_th[1,0] + result_thZero[0])/Result_th[0,0]
+    xTwo = -1*(xOne*Result_th[0,0] + Result_thZero[0])/Result_th[1,0]
     
     plt.clf()
     
     plt.plot(positive_features[:,0], positive_features[:,1], 'ro')
     plt.plot(negative_features[:,0], negative_features[:,1], 'bo')
     plt.plot(xOne, xTwo)
-    plt.axis([-1000, 3500, -0.5, 1.5])
+    plt.axis([-1000, 3500, -500, 1500])
     plt.savefig("E:\_Github\ML_linear classifiers\images\lcResult"+ str(iteration) +'.png')
     
+def ShowGraph (Result_th, Result_thZero, Dataset):
+    dataset_features, dataset_labels = DataOrganize(Dataset)
+    
+    positive_features = dataset_features[np.where(dataset_labels[:,0] == 1)]
+    negative_features = dataset_features[np.where(dataset_labels[:,0] == -1)]
+    
+    xOne = np.linspace(-1000,3500)
+    xTwo = -1*(xOne*Result_th[0,0] + Result_thZero[0])/Result_th[1,0]
+    
+    plt.clf()
+    plt.plot(positive_features[:,0], positive_features[:,1], 'ro')
+    plt.plot(negative_features[:,0], negative_features[:,1], 'bo')
+    plt.plot(xOne, xTwo)
+    plt.axis([-1000, 3500, -500, 1500])
+    
 
-def Perceptron (iteration, Dataset):   
+def Perceptron (iteration, Dataset, SaveImages):   
     dataset_features, dataset_labels = DataOrganize(Dataset)
     
     th = np.zeros(np.shape(dataset_features)[1])[np.newaxis].T
@@ -59,19 +72,21 @@ def Perceptron (iteration, Dataset):
     for t in range(iteration):
         changed = False
         for i in range(dataset_labels.shape[0]):
-            if dataset_labels[i]*(np.dot(initial_th.T, dataset_features[i]) + initial_thZero) <= 0:
-                th += (dataset_features[i]*dataset_labels[i])[np.newaxis].T
-                thZero += dataset_labels[i]
-                
-                ShowGraphAnimation(th, thZero, Dataset, (t+1)*(i+1))
-                
+            if dataset_labels[i:i+1,:]*(np.dot(th.T, dataset_features[i:i+1,:].T) + thZero) <= 0:
+                th = th + (dataset_features[i:i+1,:]*dataset_labels[i:i+1,:]).T
+                thZero = thZero + dataset_labels[i:i+1,:]
+                if SaveImages == True:
+                    SaveGraphAnimationImages(th, thZero, Dataset, (t+1)*(i+1))
                 changed = True
+                
         if changed == False:
+            print("Found Seperation")
             break
     return th, thZero
 
-result_th, result_thZero = Perceptron(8, dataset)
+result_th, result_thZero = Perceptron(3, dataset, True)
 
+ShowGraph(result_th, result_thZero, dataset)
 
 # In[ ]:
 
